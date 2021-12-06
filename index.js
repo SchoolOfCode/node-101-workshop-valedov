07121b3c5b35ba442ebc15f9bfac7826e333f740
Dovydas Lozys <reduktorius@gmail.com>
2021-12-06 16:23:19 +0000

    completed task 3

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
-let myCollection = [
-    {
-      name: "School of Code mug",
-      count: 1,
-      whatILike: "It has my cute pixel character on it!"
-    },
-    {
-      name: "School of Code hat",
-      count: 2,
-      whatILike: "An often overlooked fashion accessory"
-    },
-    {
-      name: "School of Code pillow",
-      count: 1,
-      whatILike: "Eat. Sleep. Code. Repeat :)"
-    }
-  ];
-
-//console.log(myCollection);
+import myCollection from './collection.js';
 
 function describeItem(item) {
-    //one item = "I have a name. Here's what I like about it: whatILike"
     if (item.count === 1) {
         console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`);
     } else {
-      console.log(`"I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}`)
+      console.log(`"I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}`);
     }
-    //more than one = "I have count names. Here's what I like about them: whatILike"
 }
-
-// task 2d. 
 
 function describeCollection(array) {
-  array.forEach(describeItem)
+  array.forEach(describeItem);
 }
 
-describeCollection(myCollection)
+describeCollection(myCollection);
