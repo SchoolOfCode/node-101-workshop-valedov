56e233d7afcb928641ebcd7f29fb3614ae1be0a5
valeriocipolla <valeriocipolla92@gmail.com>
2021-12-06 16:15:55 +0000

    completed task 2

--- index.js
+++ index.js
@@ -21,7 +21,17 @@ let myCollection = [
 function describeItem(item) {
     //one item = "I have a name. Here's what I like about it: whatILike"
     if (item.count === 1) {
-        console.log(`I have a ${item.name}. Here's what I like about it: whatILike`);
+        console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`);
+    } else {
+      console.log(`"I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}`)
     }
     //more than one = "I have count names. Here's what I like about them: whatILike"
 }
+
+// task 2d. 
+
+function describeCollection(array) {
+  array.forEach(describeItem)
+}
+
+describeCollection(myCollection)
